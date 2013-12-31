384d4b79f46e76f6b15671c52e969b2b3e5282c1
Simon <Alex@Alex-PC>
2013-12-31 23:52:22 +0800

    add webDriver learn

---
+++ WebDriver-learn/src/main/java/learn/Test01.java
@@ -0,0 +1,15 @@
+package learn;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Test01 {
+	
+	@Test
+	public void test1(){
+		WebDriver driver = new HtmlUnitDriver();
+		
+		
+	}
+}
